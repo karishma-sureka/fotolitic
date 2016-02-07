d450933685b58dbdadf658ac34330f8a11bf9274
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-02-07 01:46:13 -0800

    Getting the friends list. Starting with getting photos.

--- POC/FbFlask/app.py
+++ POC/FbFlask/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, url_for, session, request
 from flask_oauth import OAuth
+import logging
+from logging.handlers import RotatingFileHandler
+import urllib2
+from flask import Flask
+import requests
 
 
 SECRET_KEY = 'development key'
@@ -20,9 +25,8 @@ facebook = oauth.remote_app('facebook',
     authorize_url='https://www.facebook.com/dialog/oauth',
     consumer_key=FACEBOOK_APP_ID,
     consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email'}
+    request_token_params={'scope': 'email, user_photos, read_custom_friendlists' }
 )
-
 
 @app.route('/')
 def index():
@@ -36,6 +40,32 @@ def login():
         _external=True))
 
 
+@app.route('/friends')
+def friends():
+
+    userID = 10208654415267951
+    token = 'CAAYbAKP23MoBAEZCZCfYVYsC1mXjSoIFSBtTKDEw0ztos3DEdGUzhqL8duRQ4jt1gSrkbaQCmaPe907LwD2g1SUBK1HAR43ekgIeUW9o7WfAnOuA1YI4NDMF3UT0LYgmk5hforb7LnXGOjMlShPvl9IqUXvAHln0MpnbahzWNWVQUvBzPX82QPipWZBugFKuayE8C61ama8AkqwmXzv'
+    url = 'https://graph.facebook.com/v2.5/'+ str(userID) + "/friendlists?access_token="+token
+    # 10208654415267951/friendlists?access_token=CAAYbAKP23MoBAEZCZCfYVYsC1mXjSoIFSBtTKDEw0ztos3DEdGUzhqL8duRQ4jt1gSrkbaQCmaPe907LwD2g1SUBK1HAR43ekgIeUW9o7WfAnOuA1YI4NDMF3UT0LYgmk5hforb7LnXGOjMlShPvl9IqUXvAHln0MpnbahzWNWVQUvBzPX82QPipWZBugFKuayE8C61ama8AkqwmXzv
+    response = requests.get(url, data=[])
+    app.logger.info(response.text)
+
+    return response.text
+
+
+@app.route('/pics')
+def pics():
+
+    userID = 10208654415267951
+    token = 'CAAYbAKP23MoBAEZCZCfYVYsC1mXjSoIFSBtTKDEw0ztos3DEdGUzhqL8duRQ4jt1gSrkbaQCmaPe907LwD2g1SUBK1HAR43ekgIeUW9o7WfAnOuA1YI4NDMF3UT0LYgmk5hforb7LnXGOjMlShPvl9IqUXvAHln0MpnbahzWNWVQUvBzPX82QPipWZBugFKuayE8C61ama8AkqwmXzv'
+    url = 'https://graph.facebook.com/v2.5/'+ str(userID) + "/photos?access_token="+token
+    # 10208654415267951/friendlists?access_token=CAAYbAKP23MoBAEZCZCfYVYsC1mXjSoIFSBtTKDEw0ztos3DEdGUzhqL8duRQ4jt1gSrkbaQCmaPe907LwD2g1SUBK1HAR43ekgIeUW9o7WfAnOuA1YI4NDMF3UT0LYgmk5hforb7LnXGOjMlShPvl9IqUXvAHln0MpnbahzWNWVQUvBzPX82QPipWZBugFKuayE8C61ama8AkqwmXzv
+    response = requests.get(url, data=[])
+    app.logger.info(response.text)
+
+    return response.text
+
+
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
@@ -46,8 +76,39 @@ def facebook_authorized(resp):
         )
     session['oauth_token'] = (resp['access_token'], '')
     me = facebook.get('/me')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
+    app.logger.info(me)
+
+    url = '/v2.5/' + me.data['id'] + '/photos'
+
+    app.logger.info(url)
+    app.logger.info(resp['access_token'])
+    # facebook.request(path=url)
+
+    app.logger.info(me.data['id'])
+    # app.logger.info(facebook.request(path='/v2.5/10208654415267951/photos'))
+
+    # r = requests.get('http://www.github.com')
+
+
+    # "https://graph.facebook.com/oauth/access_token_info?client_id=" + FACEBOOK_APP_ID + "&access_token=" + session['oauth_token']
+
+    access_token = session['oauth_token']
+    app.logger.info(access_token)
+    r = requests.get("https://graph.facebook.com//v2.5/10208654415267951/photos")
+    url = "https://graph.facebook.com/oauth/access_token_info?client_id=" + FACEBOOK_APP_ID + "&access_token=" + access_token[0]
+    # data = {"access_token": session['oauth_token']}
+    response = requests.get(url, data=[])
+    app.logger.info(response.text)
+
+
+    url = "https://graph.facebook.com/v2.5/" + me.data['id'] + "/friendlists"
+    data = {"access_token": session['oauth_token']}
+    response = requests.get(url, data=data)
+    app.logger.info(response.text)
+
+    return 'Logged in as id=%s name=%s access_token=%s' % \
+        (me.data['id'], me.data['name'], session['oauth_token'])
+
 
 
 @facebook.tokengetter
@@ -56,5 +117,8 @@ def get_facebook_oauth_token():
 
 
 if __name__ == '__main__':
-    port = 8001
+    port = 8080
+    handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(port=port)
